4db4678874d9b3984b8af5dfe3bcf7fce521c7c0
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-08-19 02:38:32 +0600

    Role-based user authorization

--- src/main/java/kz/zhelezyaka/springSecurity/config/SecurityConfig.java
+++ src/main/java/kz/zhelezyaka/springSecurity/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package kz.zhelezyaka.springSecurity.config;
 
+import kz.zhelezyaka.springSecurity.model.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,7 +18,17 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
+                .antMatchers(HttpMethod.POST, "/api/**").hasRole(Role.ADMIN.name())
+                .antMatchers(HttpMethod.DELETE, "/api/**").hasRole(Role.ADMIN.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 
     @Bean
@@ -26,7 +38,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 User.builder()
                         .username("admin")
                         .password(passwordEncoder().encode("admin"))
-                        .roles("ADMIN")
+                        .roles(Role.ADMIN.name())
+                        .build(),
+                User.builder()
+                        .username("user")
+                        .password(passwordEncoder().encode("user"))
+                        .roles(Role.USER.name())
                         .build()
         );
     }
---
+++ src/main/java/kz/zhelezyaka/springSecurity/model/Role.java
@@ -0,0 +1,6 @@
+package kz.zhelezyaka.springSecurity.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
