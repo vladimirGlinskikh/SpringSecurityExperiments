ec2db70c476ac45749e2212fc954401f1e9740b5
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-08-19 00:49:43 +0600

    Creating a basic project

---
+++ src/main/java/kz/zhelezyaka/springSecurity/model/Developer.java
@@ -0,0 +1,12 @@
+package kz.zhelezyaka.springSecurity.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Developer {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
---
+++ src/main/java/kz/zhelezyaka/springSecurity/rest/DeveloperRestControllerV1.java
@@ -0,0 +1,33 @@
+package kz.zhelezyaka.springSecurity.rest;
+
+import kz.zhelezyaka.springSecurity.model.Developer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperRestControllerV1 {
+    private List<Developer> developers = Stream.of(
+            new Developer(1L, "Vladimir", "Glinskikh"),
+            new Developer(2L, "Alexandr", "Baglay"),
+            new Developer(3L, "Dmitry", "Roy")
+    ).collect(Collectors.toList());
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return developers;
+    }
+
+    @GetMapping("/{id}")
+    public Developer getById(@PathVariable Long id) {
+        return developers.stream().filter(developer -> developer.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+}
