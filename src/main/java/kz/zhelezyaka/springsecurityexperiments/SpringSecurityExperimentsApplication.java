04c9e2d6d1c45cb939ac1df0a18531aabc855359
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-05-12 21:47:23 +0600

    Start project

---
+++ src/main/java/kz/zhelezyaka/springsecurityexperiments/SpringSecurityExperimentsApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.springsecurityexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityExperimentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityExperimentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/zhelezyaka/springsecurityexperiments/SpringSecurityExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.springsecurityexperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExperimentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
