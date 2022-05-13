a70f58bed3dc04ea321ba3e0a6e19ba0d904db19
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-05-13 06:47:36 +0600

    Overriding the UserDetailsService component

---
+++ src/main/java/kz/zhelezyaka/springsecurityexperiments/config/ProjectConfig.java
@@ -0,0 +1,30 @@
+package kz.zhelezyaka.springsecurityexperiments.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsService = new InMemoryUserDetailsManager();
+
+        var user = User.withUsername("Vladimir")
+                .password("123")
+                .authorities("read")
+                .build();
+        userDetailsService.createUser(user);
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
