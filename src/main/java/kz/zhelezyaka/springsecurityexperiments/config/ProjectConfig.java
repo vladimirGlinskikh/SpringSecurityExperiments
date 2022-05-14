36a90be707628dcf55eefc649bb54d337a2f92f2
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-05-14 22:03:10 +0600

    Setting UserDetailsService and PasswordEncoder in configure()

--- src/main/java/kz/zhelezyaka/springsecurityexperiments/config/ProjectConfig.java
+++ src/main/java/kz/zhelezyaka/springsecurityexperiments/config/ProjectConfig.java
@@ -2,19 +2,18 @@ package kz.zhelezyaka.springsecurityexperiments.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         var userDetailsService = new InMemoryUserDetailsManager();
 
         var user = User.withUsername("Vladimir")
@@ -22,7 +21,8 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
                 .authorities("read")
                 .build();
         userDetailsService.createUser(user);
-        return userDetailsService;
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
     @Override
@@ -30,11 +30,6 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
         http.httpBasic();
         http.authorizeRequests()
                 .anyRequest()
-                .permitAll();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+                .authenticated();
     }
 }
