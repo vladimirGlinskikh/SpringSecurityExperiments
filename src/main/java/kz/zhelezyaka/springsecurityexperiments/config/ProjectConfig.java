90a0f0a737c2784d0259a533ed9e4590247e0f44
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-05-14 23:46:03 +0600

    Overriding the AuthenticationProvider implementation

--- src/main/java/kz/zhelezyaka/springsecurityexperiments/config/ProjectConfig.java
+++ src/main/java/kz/zhelezyaka/springsecurityexperiments/config/ProjectConfig.java
@@ -1,22 +1,21 @@
 package kz.zhelezyaka.springsecurityexperiments.config;
 
+import kz.zhelezyaka.springsecurityexperiments.security.CustomAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private CustomAuthenticationProvider authenticationProvider;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("Vladimir")
-                .password("123")
-                .authorities("read")
-                .and()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+        auth.authenticationProvider(authenticationProvider);
     }
 
     @Override
