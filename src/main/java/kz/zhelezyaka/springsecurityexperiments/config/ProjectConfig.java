b12e1409e35e6158f8c69b775e0136af945a64ed
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-05-15 07:11:40 +0600

    Implementing the UserDetailsService contract

--- src/main/java/kz/zhelezyaka/springsecurityexperiments/config/ProjectConfig.java
+++ src/main/java/kz/zhelezyaka/springsecurityexperiments/config/ProjectConfig.java
@@ -1,28 +1,29 @@
 package kz.zhelezyaka.springsecurityexperiments.config;
 
-import kz.zhelezyaka.springsecurityexperiments.security.CustomAuthenticationProvider;
-import org.springframework.beans.factory.annotation.Autowired;
+import kz.zhelezyaka.springsecurityexperiments.model.User;
+import kz.zhelezyaka.springsecurityexperiments.services.InMemoryUserDetailsService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
 
 @Configuration
-public class ProjectConfig extends WebSecurityConfigurerAdapter {
+public class ProjectConfig {
 
-    @Autowired
-    private CustomAuthenticationProvider authenticationProvider;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider);
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = new User("Vladimir", "123", "read");
+        UserDetails userDetails1 = new User("Svetlana", "124", "write");
+        List<UserDetails> users = List.of(userDetails, userDetails1);
+        return new InMemoryUserDetailsService(users);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
-        http.authorizeRequests()
-                .anyRequest()
-                .authenticated();
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
