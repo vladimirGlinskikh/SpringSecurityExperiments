184ba1f94729fd8df46a8b59cb3ff7b53fc16aa5
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-05-16 20:57:45 +0600

    Create branch for experiment with delete file

--- src/main/java/kz/zhelezyaka/springsecurityexperiments/config/ProjectConfig.java
+++ src/main/java/kz/zhelezyaka/springsecurityexperiments/config/ProjectConfig.java
@@ -1,25 +1,21 @@
 package kz.zhelezyaka.springsecurityexperiments.config;
 
-import kz.zhelezyaka.springsecurityexperiments.model.User;
-import kz.zhelezyaka.springsecurityexperiments.services.InMemoryUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import java.util.List;
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectConfig {
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails userDetails = new User("Vladimir", "123", "read");
-        UserDetails userDetails1 = new User("Svetlana", "124", "write");
-        List<UserDetails> users = List.of(userDetails, userDetails1);
-        return new InMemoryUserDetailsService(users);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS spring;
+CREATE DATABASE IF NOT EXISTS spring;
+
+USE spring;
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id       INT         NOT NULL AUTO_INCREMENT,
+    username VARCHAR(45) NOT NULL,
+    password VARCHAR(45) NOT NULL,
+    enabled  INT         NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS authorities
+(
+    id        INT         NOT NULL AUTO_INCREMENT,
+    username  VARCHAR(45) NOT NULL,
+    authority VARCHAR(45) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT IGNORE INTO authorities VALUES (NULL, 'Vladimir', 'write');
+INSERT IGNORE INTO authorities VALUES (NULL, 'Svetlana', 'write');
+INSERT IGNORE INTO users VALUES (NULL, 'Vladimir', '123', '1');
+INSERT IGNORE INTO users VALUES (NULL, 'Svetlana', '124', '1');
+
+SELECT * FROM users;
+SELECT * FROM authorities;
+
+
+
